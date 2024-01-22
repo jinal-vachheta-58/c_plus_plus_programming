172c4eb6f2ef5939e95496b2ef7f2480bb002fdd
Jinal P. Vachheta <156422920+jinal-vachheta-58@users.noreply.github.com>
2024-01-22 17:58:20 +0530

    Compute quotient and remainder

---
+++ 7.cpp
@@ -0,0 +1,29 @@
+// Compute quotient and remainder
+
+#include<iostream>
+using namespace std;
+
+int main()
+{    
+    int divisor, dividend, quotient, remainder;
+
+    cout << "Enter dividend: ";
+    cin >> dividend;
+
+    cout << "Enter divisor: ";
+    cin >> divisor;
+
+    quotient = dividend / divisor;
+    remainder = dividend % divisor;
+
+    cout << "Quotient = " << quotient << endl;
+    cout << "Remainder = " << remainder;
+
+    return 0;
+}
+
+// output:
+// Enter dividend: 13
+// Enter divisor: 4
+// Quotient = 3
+// Remainder = 1
