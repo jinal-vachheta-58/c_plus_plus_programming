b4ed4e9d9598201b6e9694cb9bd020f18441da5a
Jinal P. Vachheta <156422920+jinal-vachheta-58@users.noreply.github.com>
2024-01-22 17:58:37 +0530

    Find the Factorial

---
+++ 8.cpp
@@ -0,0 +1,26 @@
+// Find the Factorial of a Given Number
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    long factorial = 1.0;
+
+    cout << "Enter a positive integer: ";
+    cin >> n;
+
+    if (n < 0)
+        cout << "Error! Factorial of a negative number doesn't exist.";
+    else {
+        for(int i = 1; i <= n; ++i) {
+            factorial *= i;
+        }
+        cout << "Factorial of " << n << " = " << factorial;    
+    }
+
+    return 0;
+}
+
+// output:
+// Enter a positive integer: 4
+// Factorial of 4 = 24
