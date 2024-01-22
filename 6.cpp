a8f0a0013933a109a42fceb935dc2a665b789717
Jinal P. Vachheta <156422920+jinal-vachheta-58@users.noreply.github.com>
2024-01-22 17:58:01 +0530

    Find Size of variable

---
+++ 6.cpp
@@ -0,0 +1,20 @@
+// C++ Program to Find Size of variable
+
+#include<iostream>
+using namespace std;
+
+int main() 
+{    
+    cout << "Size of char: " << sizeof(char) << " byte" << endl;
+    cout << "Size of int: " << sizeof(int) << " bytes" << endl;
+    cout << "Size of float: " << sizeof(float) << " bytes" << endl;
+    cout << "Size of double: " << sizeof(double) << " bytes" << endl;
+
+    return 0;
+}
+
+// output:
+// Size of char: 1 byte
+// Size of int: 4 bytes
+// Size of float: 4 bytes
+// Size of double: 8 bytes
