2c41f447744b16ff1c98eae04c8d321472ede9a1
Jinal P. Vachheta <156422920+jinal-vachheta-58@users.noreply.github.com>
2024-07-27 16:03:06 +0530

    Create ch2_1.cpp

---
+++ ch2_1.cpp
@@ -0,0 +1,22 @@
+// 2.1 Write a program to display the following output using a single cout statement
+// 			Maths 	   = 90
+// 			Physics    = 77
+// 			Chemistry  = 69
+// ...........................................................................................
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"Maths = 90"<<endl<<"physics = 77"<<endl<<"chemistry = 69";
+}
+
+
+// >>>>>>>>>
+// OUTPUT:
+// >>>>>>>>>
+
+
+// Maths = 90
+// physics = 77
+// chemistry = 69
+// --------------------------------
