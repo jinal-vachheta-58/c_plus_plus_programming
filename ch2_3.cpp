845e5aef881ee2c2a448239f210ce785f327ab9f
Jinal P. Vachheta <156422920+jinal-vachheta-58@users.noreply.github.com>
2024-07-27 16:05:47 +0530

    Create ch2_3.cpp

---
+++ ch2_3.cpp
@@ -0,0 +1,38 @@
+
+// ..................................................................................................................
+// 2.3 Write a program to input an integer value from keyboard and display on screen "WELL DONE" that many times .
+// ...................................................................................................................
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int k,i;
+	cout<<"Enter number : ";
+	cin>>k;
+	
+	for(i=0;i<k;i++)
+	{
+		cout<<"WELL DONE"<<endl;
+	}
+}
+
+
+// >>>>>>>>>
+// OUTPUT:
+// >>>>>>>>>
+
+// Enter number : 10
+// WELL DONE
+// WELL DONE
+// WELL DONE
+// WELL DONE
+// WELL DONE
+// WELL DONE
+// WELL DONE
+// WELL DONE
+// WELL DONE
+// WELL DONE
+
+// --------------------------------
+
